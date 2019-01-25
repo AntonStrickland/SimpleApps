b3cdb820316f6d8ab6d8ad5cc0ac3af87433a746
Kinjo <antonmstrickland@gmail.com>
2019-01-25 01:32:00 -0600

    Added comments

--- Words.cpp
+++ Words.cpp
@@ -11,14 +11,14 @@ void Words::Run()
 void Words::Reverse()
 {
 	string theString = "";
+	string newString = "";
+	int wordStartIndex = 0;
+
 	cout << "This program will reverse every word in a string, but keep the words in order." << endl;
 	cout << "Please enter a string: " << endl;
 	getline(cin, theString);
 
-	string newString = "";
-
-	int wordStartIndex = 0;
-
+	// If the final word does not end with a space, add an extra space to the end
 	if (theString[theString.length() - 1] != ' ')
 		theString += ' ';
 
@@ -26,8 +26,10 @@ void Words::Reverse()
 
 	for (int i = 0; i < length; i++)
 	{
+		// Move forward until we reach the end of a word
 		if (theString[i] == ' ')
 		{
+			// Add that word backwards into the new string
 			for (int k = i; k >= wordStartIndex; k--)
 			{
 				newString += theString[k];
