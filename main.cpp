ad4dcf0b02f052d7f0e6a09e010a27421dd49089
Kinjo <antonmstrickland@gmail.com>
2019-01-25 00:23:28 -0600

    Can press 1 to run the program again

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 // Programmer: Anton Strickland
 
 #include "Palindrome.h"
+#include <iostream>
+using namespace std;
+
+bool ShouldRunAgain();
 
 int main()
 {
-	SimpleApp* app = new Palindrome();
-	app->Run();
-	delete app;
+	do
+	{
+		SimpleApp* app = new Palindrome();
+		app->Run();
+		delete app;
+	} while (ShouldRunAgain());
+
 	return 0;
+}
+
+bool ShouldRunAgain()
+{
+	cout << "Press 1 to run again, or any other key to exit." << endl;
+	int key = 0;
+	cin >> key;
+	return (key == 1);
 }
